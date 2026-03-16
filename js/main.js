9d97595814b80d4920de44b2d5c1c594f8b4036c
sgrauc2324-png <sgrauc2324@politecnics.barcelona>
2026-03-16 09:17:47 +0100

    alibaba

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@
 // 1. CARGAR HEADER Y FOOTER
 // ==========================================
 
-fetch('header.html').then(res => res.text()).then(data => { const h = document.getElementById('main-header'); if (h) h.innerHTML = data; }).catch(e => console.error('Error header:', e));
-fetch('footer.html').then(res => res.text()).then(data => { const f = document.getElementById('main-footer'); if (f) f.innerHTML = data; }).catch(e => console.error('Error footer:', e));
+fetch('header.html').then(res => res.text()).then(data => { const h = document.getElementById('header'); if (h) h.innerHTML = data; }).catch(e => console.error('Error header:', e));
+fetch('footer.html').then(res => res.text()).then(data => { const f = document.getElementById('footer'); if (f) f.innerHTML = data; }).catch(e => console.error('Error footer:', e));
